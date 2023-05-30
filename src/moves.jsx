320c83d1bfc0afb8493b398c124a9987983e8e63
Reuven Peleg <reuven.peleg@orcam.com>
2023-05-30 08:52:02 +0300

    Show move numbering

--- src/moves.jsx
+++ src/moves.jsx
@@ -5,11 +5,14 @@ const moveDivStyle = {
     width: '100px',
     height: '40px',
     backgroundColor: 'rgb(220, 220, 220)',
+    color: 'black',
     margin: '5px',
 }
 
 const moveContainerStyle = {
-    display: 'flex', flexDirection: 'row'
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
 }
 
 const toPieceSymbol = (san) => {
@@ -25,11 +28,27 @@ const toPieceSymbol = (san) => {
     });
 }
 
-const MovesDisplay = ({moves, totalMoveCount}) => {
+const splitToPairs = (array) => array.reduce((accumulator, currentValue, index) => {
+    if (index % 2 === 0) {
+        accumulator.push([currentValue, array[index + 1]]);
+    }
+
+    return accumulator;
+}, []);
+
+const MovesDisplay = ({ moves, totalMoveCount }) => {
     const emptySpaces = Math.max(0, totalMoveCount - moves.length);
+    const firstMovePlaceholder = totalMoveCount % 2 == 0 ? [<div style={moveDivStyle}>...</div>] : [];
+    const movesDivs = moves.map(m => <div style={moveDivStyle}>{toPieceSymbol(m.san)}</div>);
+    const emptySpacesDiv = Array.from(Array(emptySpaces), () => <div style={moveDivStyle}></div>);
+    const allDivs = firstMovePlaceholder.concat(movesDivs, emptySpacesDiv)
+    const divPairs = splitToPairs(allDivs)
     return <div style={moveContainerStyle}>
-        {moves.map(m => <div style={moveDivStyle}>{toPieceSymbol(m.san)}</div>)}
-        {Array.from(Array(emptySpaces), () => <div style={moveDivStyle}></div>)}
+        {divPairs.map((pair, idx) => <>
+            {idx + 1}.
+            {pair[0]}
+            {pair[1]}
+        </>)}
     </div>
 };
 
