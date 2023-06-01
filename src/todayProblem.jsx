4ee51deb3c18455ef2d0407e4382a124ed6d0135
Reuven Peleg <reuven.peleg@orcam.com>
2023-06-01 11:47:15 +0300

    Fetch problem of the day once

--- src/todayProblem.jsx
+++ src/todayProblem.jsx
@@ -1,35 +1,39 @@
 import HelpmateProblem from './problem'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 const TodayProblem = () => {
-  const todayString = new Date().toISOString().slice(0, 10);
-  const todayProblemUrl = `https://raw.githubusercontent.com/R-Peleg/daily-helpmate/main/problems/${todayString}.json`
+    const todayString = new Date().toISOString().slice(0, 10);
+    const todayProblemUrl = `https://raw.githubusercontent.com/R-Peleg/daily-helpmate/main/problems/${todayString}.json`
 
-  const [problemJson, setProblemJson] = useState(null);
-  const [status, setStatus] = useState('request');
-  
-  fetch(todayProblemUrl)
-  .then(response => response.json())
-  .then(json => {
-    setProblemJson(json);
-    setStatus('success');
-  })
+    const [problemJson, setProblemJson] = useState(null);
+    const [status, setStatus] = useState('request');
 
-  // Bridge FEN format
-  if (problemJson && problemJson.fen.split(' ').length === 1) {
-    const color = Number.isInteger(problemJson.moves) ? 'b' : 'w';
-    problemJson.fen = problemJson.fen + ' ' + color + ' - - 0 1';
-  }
+    useEffect(() => {
+        if (!problemJson) {
+            fetch(todayProblemUrl)
+                .then(response => response.json())
+                .then(json => {
+                    setProblemJson(json);
+                    setStatus('success');
+                })
+        }
+    }, [])
 
-  return <>
-      {problemJson && <HelpmateProblem 
-        initialFen={problemJson.fen}
-        moveCount={problemJson.moves * 2}
-        solutions={problemJson.solutions}
-        variants={problemJson.variants}
-        author={problemJson.author}
-        year={problemJson.year}/>}
+    // Bridge FEN format
+    if (problemJson && problemJson.fen.split(' ').length === 1) {
+        const color = Number.isInteger(problemJson.moves) ? 'b' : 'w';
+        problemJson.fen = problemJson.fen + ' ' + color + ' - - 0 1';
+    }
+
+    return <>
+        {problemJson && <HelpmateProblem
+            initialFen={problemJson.fen}
+            moveCount={problemJson.moves * 2}
+            solutions={problemJson.solutions}
+            variants={problemJson.variants}
+            author={problemJson.author}
+            year={problemJson.year} />}
     </>
 }
 
